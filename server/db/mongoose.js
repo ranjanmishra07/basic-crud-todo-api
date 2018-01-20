0584ccb8a6634cfdd779a8135e55b921016bf642
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-01-20 22:13:23 +0530

    added heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:/27017/TodoApp');
+mongoose.connect('mongodb://mishra.ranjan5:Ranjan07@ds263847.mlab.com:63847/todoapi');
+// 'mongodb://localhost:/27017/TodoApp'
 
 module.exports={mongoose};
-
-    	
