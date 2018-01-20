14d3cffa6ad195dcbb78aa00f7b445747012f99d
Ranjan Mishra <mishra.ranjan5@gmail.com>
2018-01-21 00:13:24 +0530

    updated database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://raja:raja.mlab.com:63847/todoapi');
+mongoose.connect('mongodb://mishra.ranjan5:ratialgroup07@ds263707.mlab.com:63707/crudtodo');
 // mongoose.connect('mongodb://localhost:/27017/TodoApp');
 
 
